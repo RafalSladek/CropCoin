7ea96dcd5b664e6e816a9227f17a410539c4da7d
Rafal <rafal.sladek@gmail.com>
2018-03-02 18:59:33 +0100

    splitted docker into base and latest

---
+++ buildImage-full.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -f Dockerfile -t rafalsladek/cropcoin-masternode:latest .
--- pushImage.sh
+++ pushImage.sh
@@ -1,7 +1,8 @@
-$ cat pushImage.sh
 #!/bin/bash
 echo "You should be logged in dockerhub before run this script"
 echo 
+tag=base
+docker push rafalsladek/cropcoin-masternode:$tag
+
 tag=latest
-docker tag cropcoin-masternode:$tag rafalsladek/cropcoin-masternode:$tag
 docker push rafalsladek/cropcoin-masternode:$tag
