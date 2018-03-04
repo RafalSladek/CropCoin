b5eb4580b80842e1309e46cdaf37e078a3c0f0ae
Rafal <rafal.sladek@gmail.com>
2018-03-04 20:15:46 +0100

    cleanup repo

--- cleanupDocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker ps -a && \
-docker stop cropcoin-wallet && \
-docker rm cropcoin-wallet && \
-docker rm $(docker ps -qa --no-trunc --filter "status=exited") && \
-docker rm $(docker ps -qa --no-trunc --filter "status=created") && \
-docker images | grep "none" && \
-docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
